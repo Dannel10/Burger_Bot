468fb9d1fdd8ca63b65c949d01c254ff7efea43e
Dannel10 <20150@my.sanctamaria.school.nz>
2023-06-22 14:43:07 +1200

    Created Component 2 delivery/click and collect menu
    Created Component 2 delivery/click and collect menu

---
+++ Components/click_delivery_menu v1.py
@@ -0,0 +1,16 @@
+#Bugs
+#Will only work for vaild input "d" and "c"
+
+#Menu so that user can choose pick up or delivery
+
+print ("Do you want your order delivered or do you want click and collect?")
+
+print (" For delivery enter d or enter c for click and collect")
+
+delivery = input()
+
+if delivery == "d":
+    print ("Delivery")
+           
+elif delivery == "c":
+    print ("Click and Collect")
