c41b1d9f1d2177d2e79583814c3dcf965b4dc183
Dannel10 <20150@my.sanctamaria.school.nz>
2023-08-09 22:52:06 +1200

    Integrated new order or exit
    Integrated new order or exit function. Component 9, now able to confirm or cancel order and start another order or exit program

--- burger_bot .py
+++ burger_bot .py
@@ -1,6 +1,8 @@
 # Burger Bot
 # Bugs - Phone Number allows letters
 #      - Name allows numbers
+
+import sys
 import random
 from random import randint
 
@@ -196,26 +198,32 @@ def print_order(del_pick):
             total_cost = total_cost + 9
     print("Total Order Cost")
     print(f"${total_cost:.2f}")
-
+    print()
     
+
 # Ability to cancel or proceed with order
 def confirm_order():
     print ("Please Confirm Your Order")
     print ("To Confirm please enter 1") 
     print ("To Cancel please enter 2")
+    print()
     while True:
         try:
             confirm = int(input("Please enter a number "))
             if confirm >= 1 and confirm <= 2:
                 if confirm == 1:
                     print ("Order Confirmed")
-                    print ("Your burger is being made")
+                    print ("Your order will be sent to our Store")
                     print ("It will be ready soon")
+                    print()
+                    new_exit()
                     break
 
                 elif confirm == 2:
                     print ("Order Cancelled")
                     print ("You can restart your order or exit the BOT")
+                    print()
+                    new_exit()
                     break
             else:
                 print("Number must be 1 or 2")
@@ -223,8 +231,38 @@ def confirm_order():
             print ("That was not a valid input")
             print ("Please enter 1 or 2 ")
 
-# Option for new order or to exit
 
+# Option for new order or to exit
+def new_exit():
+    print ("Do you want to start another Order or Exit?")
+    print ("To start another order enter 1") 
+    print ("To exit the bot enter 2")
+    while True:
+        try:
+            confirm = int(input("Please enter a number "))
+            if confirm >= 1 and confirm <= 2:
+                if confirm == 1:
+                    print ("New Order")
+                    print()
+                    order_list.clear()
+                    order_cost.clear()
+                    customer_details.clear()
+                    main()
+                    break
+
+
+                elif confirm == 2:
+                    print ("Exit")
+                    order_list.clear()
+                    order_cost.clear()
+                    customer_details.clear()
+                    sys.exit
+                    break
+            else:
+                print("Number must be 1 or 2")
+        except ValueError:
+            print ("That was not a valid input")
+            print ("Please enter 1 or 2 ")
 # Main Function
 def main():
     '''
